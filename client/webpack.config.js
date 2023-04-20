c954bde094d08a375c54786c1bd43d1f426a96c5
Dyllan <dphamada92@gmail.com>
2023-04-20 14:32:27 -0700

    was able to get server to start up, but have to fix some of the logic

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -30,17 +30,9 @@ module.exports = () => {
         crossorigin: "use-credentials", //can be null, use-credentials or anonymous
         icons: [
           {
-            src: path.resolve("src/assets/icon.png"),
-            sizes: [96, 128, 192, 256, 384, 512], // multiple sizes
-          },
-          {
-            src: path.resolve("src/assets/large-icon.png"),
-            size: "1024x1024", // you can also use the specifications pattern
-          },
-          {
-            src: path.resolve("src/assets/maskable-icon.png"),
-            size: "1024x1024",
-            purpose: "maskable",
+            src: path.resolve("src/images/logo.png"),
+            sizes: [96, 128, 192, 256, 384, 512], 
+            destination: path.join('assests', 'icon'), // multiple sizes
           },
         ],
       }),
